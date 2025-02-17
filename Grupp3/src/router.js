4bdadb1c2b089b4ecc09d5971d3e3f0ef1d7ccee
Maryam <maryam.chaudhry@iths.se>
2025-02-17 11:01:50 +0100

    installerat vue/vite

---
+++ Grupp3/src/router.js
@@ -0,0 +1,6 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./components/Home.vue";
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: [{}],
+});
