4d084ef8afbacaa974d4c05294796b2f7dd89731
Maryam <maryam.chaudhry@iths.se>
2025-02-17 14:23:19 +0100

    Lagt till komponent i router

--- Grupp3/src/router.js
+++ Grupp3/src/router.js
@@ -2,5 +2,10 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./components/Home.vue";
 export default createRouter({
   history: createWebHashHistory(),
-  routes: [{}],
+  routes: [
+    {
+      component: Home,
+      path: "/",
+    },
+  ],
 });
