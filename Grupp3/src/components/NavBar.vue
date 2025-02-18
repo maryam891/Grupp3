c549bf677529a18184728f88476d358ba2a18d75
Fiona Bui <buiphf@gmail.com>
2025-02-18 13:03:35 +0100

    updateras assets

--- Grupp3/src/components/NavBar.vue
+++ Grupp3/src/components/NavBar.vue
@@ -1,19 +1,35 @@
 <script setup></script>
 
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/quiz">Quiz</router-link>
-      </li>
-      <li>
-        <router-link to="/facts">Facts</router-link>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <nav class="navbar-container">
+      <router-link to="/">
+        <img src="/assets/image/Space Learning Logo.png" alt="Logo" />
+      </router-link>
+      <ul class="nav">
+        <li class="nav-link">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="nav-link">
+          <router-link to="/quiz">Quiz</router-link>
+        </li>
+        <li class="nav-link">
+          <router-link to="/facts">Facts</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
-<style></style>
+<style>
+.navbar-container {
+  display: flex;
+  justify-content: space-around;
+}
+.nav {
+  display: flex;
+}
+.nav-link {
+  padding: 0 20px;
+}
+</style>
