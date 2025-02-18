e2f7088c8cb68794f25924436f66d50bb53bcc20
Emilie <emiliebn@outlook.com>
2025-02-18 13:26:16 +0100

    skapar homeview-sidan

--- Grupp3/src/views/HomeView.vue
+++ Grupp3/src/views/HomeView.vue
@@ -1,20 +1,31 @@
-<script>
-export default {
-  created() {
-    fetch("https://avancera.app/cities/")
-      .then((response) => response.json())
-      .then((cities) => {
-        this.cities = cities;
-      });
-  },
-  data() {
-    return {
-      cities: null,
-    };
-  },
-};
-</script>
-
 <template>
-  <ol></ol>
+  <main>
+    <div class="startpage-intro-container">
+      <h1>Learn about the planets</h1>
+      <p>One quiz at a time</p>
+      <input type="button" value="Go to quiz">
+      <input type="button" value="Read more">
+    </div>
+    <div class="startpage-sellingpoints-container">
+      <img src="" alt="Fact 1">
+      <p>Gamified learning</p>
+      <img src="" alt="Fact 2">
+      <p>Bite-sized, anytime learning</p>
+      <img src="" alt="Fact 3">
+      <p>Daily challenges</p>
+    </div>
+  </main>
 </template>
+
+<style>
+body {
+  color: white;
+}
+
+.startpage-sellingpoints-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  padding: 5em;
+}
+</style>
