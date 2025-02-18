4bcda4b14e0fb5323b2ba6838170b5053b9d886a
Emilie <emiliebn@outlook.com>
2025-02-18 14:32:28 +0100

    lägger till css på homeview

--- Grupp3/src/views/HomeView.vue
+++ Grupp3/src/views/HomeView.vue
@@ -3,8 +3,10 @@
     <div class="startpage-intro-container">
       <h1>Learn about the planets</h1>
       <p>One quiz at a time</p>
-      <input type="button" value="Go to quiz">
-      <input type="button" value="Read more">
+      <div class="startpage-btn-part">
+      <input type="button" value="Go to quiz" class="btn-quiz">
+      <input type="button" value="Read more" class="btn-more">
+      </div>
     </div>
     <div class="startpage-sellingpoints-container">
       <img src="" alt="Fact 1">
@@ -18,14 +20,21 @@
 </template>
 
 <style>
-body {
-  color: white;
+.startpage-intro-container {
+  margin-top: 3em;
+  padding: 3em;
+}
+
+.btn-quiz, .btn-more {
+  margin: 2em 2em 0 0;
+  padding: .5em;
+  cursor: pointer;
 }
 
 .startpage-sellingpoints-container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
-  padding: 5em;
+  padding: 6em;
 }
 </style>
