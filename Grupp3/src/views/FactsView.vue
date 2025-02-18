6d23c1286cb33fa651c7d8e37f8581400027306f
Janefur <a.janefur@gmail.com>
2025-02-18 15:37:43 +0100

    lagt till db.json fil

--- Grupp3/src/views/FactsView.vue
+++ Grupp3/src/views/FactsView.vue
@@ -1,12 +1,5 @@
 <script>
 export default {
-  created() {
-    fetch("https://avancera.app/cities/")
-      .then((response) => response.json())
-      .then((cities) => {
-        this.cities = cities;
-      });
-  },
   data() {
     return {
       cities: null,
@@ -16,5 +9,51 @@ export default {
 </script>
 
 <template>
-  <ol></ol>
+
+  <div class="information">
+    <h1>Here are some fun facts about each planet in our solar system, including Pluto:</h1>
+    <div class="planet-info">
+
+      Mercury: The smallest planet and closest to the Sun, Mercury experiences extreme temperature fluctuations, ranging from scorching heat during the day to freezing cold at night.
+    </div>
+    <div class="planet-info">
+      Venus: Similar in size to Earth, Venus has a toxic atmosphere rich in carbon dioxide and surface temperatures hot enough to melt lead, making it the hottest planet.
+    </div>
+
+    <div class="planet-info">
+      Earth: The only known planet to support life, Earth's surface is 71% water. It has a protective atmosphere that shields it from harmful solar radiation.
+    </div>
+
+    <div class="planet-info">
+      Mars: Known as the Red Planet due to its iron oxide-rich soil, Mars hosts the largest volcano in the solar system, Olympus Mons, and has seasons similar to Earth.
+    </div>
+
+    <div class="planet-info">
+      Jupiter: The largest planet, Jupiter is famous for its Great Red Spot, a massive storm system larger than Earth, and has at least 79 moons, including the large Galilean moons.
+    </div>
+
+    <div class="planet-info">
+      Saturn: Distinguished by its prominent ring system composed of ice particles, rocky debris, and dust, Saturn is less dense than water, meaning it would float if placed in a large enough body of water.
+    </div>
+
+    <div class="planet-info">
+      Uranus: Unique for its tilted axis, Uranus rotates on its side, leading to extreme seasonal variations. Its atmosphere contains methane, giving it a blue-green color.
+    </div>
+
+    <div class="planet-info">
+      Neptune: The farthest known planet from the Sun, Neptune has the fastest winds in the solar system, reaching speeds over 1,200 miles per hour (2,000 kilometers per hour).
+    </div>
+
+    <div class="planet-info">
+      Pluto: Once considered the ninth planet, Pluto is now classified as a dwarf planet. It has five known moons, with Charon being the largest, nearly half the size of Pluto itself.
+    </div>
+
+    <p>science.nasa.gov (källa)</p>
+
+  </div>
+
 </template>
+
+<style>
+
+</style>
