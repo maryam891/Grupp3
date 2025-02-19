c35813a281ca61323d89182d916478b7b0a9fef1
Maryam <maryam.chaudhry@iths.se>
2025-02-19 14:28:06 +0100

    lägger in ändringar i quiz startsidan

--- Grupp3/src/views/QuizView.vue
+++ Grupp3/src/views/QuizView.vue
@@ -2,14 +2,17 @@
 export default {
   data() {
     return {
-      earthImg:"/assets/image/earth-transparent.png"
+      earthImg:"/assets/image/earth-transparent.png",
+      venusImg: "/assets/image/venus-transparent.png",
+      mercuryImg:"/assets/image/mercury.png",
+      starImg:"/assets/image/starImg.png"
     };
   },
 };
 </script>
 
 <template>
-<p>Test your space skills with this quiz!</p>
+<h2 >Test your space skills with this quiz!</h2>
   <div class="startQuizContainer">
     <h1>
       Quiz
@@ -17,7 +20,26 @@ export default {
     <button>
       Start Quiz
     </button>
+    <img :src="mercuryImg" class="mercuryImg">
+    <img :src="venusImg" class="venusImg">
     <img :src="earthImg" class="earthImg">
+  </div>
+  <div id="textContainer">
+  <div>
+    <p class="factText">Fact 1</p>
+    <img :src="starImg" class="starImg">
+    <p>Gamified learning</p>
+  </div>
+  <div>
+    <p class="factText">Fact 2</p>
+    <img :src="starImg" class="starImg">
+    <p>Daily challenges</p>
+  </div>
+  <div>
+    <p class="factText">Fact 3</p>
+    <img :src="starImg" class="starImg">
+    <p>Bite-sized, anytime learning</p>
+  </div>
   </div>
 </template>
 
@@ -31,20 +53,21 @@ h1 {
   font-weight: 500;
 }
 .startQuizContainer {
-  height: 247.98px;
-  width: 455px;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.518);
+  height: 260px;
+  width: 460px;
   background-color: #FDFDFD;
   position: absolute;
-  left: 23%;
-  top: 30%;
+  left: 30%;
+  top: 25%;
   border-radius: 43.02px;
 
 }
-p {
+h2 {
   font-weight: bold;
   position: absolute;
-  top: 20%;
-  left: 24%;
+  top: 10%;
+  left: 30%;
   margin-bottom: 10px;
 
 }
@@ -57,14 +80,61 @@ button {
   color:#280070;
   font-weight: 550;
   position: absolute;
-  top: 43%;
+  top: 40%;
   left: 28%;
+  cursor: pointer;
 }
 .earthImg {
-  height: 200px;
-  width: 200px;
+  height: 180px;
+  width: 180px;
   position: relative;
-  left: 64%;
-  top:40%
+  left: 20%;
+  top: 50%
 }
+.venusImg {
+
+  height: 120px;
+  width: 120px;
+  position: relative;
+  top: 40%;
+  left: -40%;
+}
+.mercuryImg {
+  height: 130px;
+  width: 130px;
+  position: relative;
+  left: -12%;
+  top:-35%
+}
+
+#textContainer {
+  display: flex;
+  width: 800px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 15px;
+  padding: 10px;
+  position: absolute;
+  top: 73%;
+  left: 22%;
+}
+
+.factText {
+  width: 90px;
+  position: relative;
+  left: 10%;
+  height: 30px;
+  background-color:#E3CAFB;
+  color: black;
+  text-align: center;
+  border-radius: 5px;
+  margin-bottom: 15px;
+}
+
+.starImg {
+  position: relative;
+  left: 25%;
+  margin-bottom: 10px;
+}
+
 </style>
