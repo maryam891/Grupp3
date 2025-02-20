fe266540570e0de1a832f68582b3a9fcc0453aa9
Emilie <emiliebn@outlook.com>
2025-02-20 14:32:55 +0100

    Lägger in styling för startsidan

--- Grupp3/src/views/HomeView.vue
+++ Grupp3/src/views/HomeView.vue
@@ -1,34 +1,105 @@
+<script>
+  export default {
+    data() {
+      return {
+        earthImg:"/assets/image/earth-transparent.png",
+        star: "/assets/image/starImg.png"
+      }
+    }
+  };
+</script>
+
 <template>
   <main>
     <div class="startpage-intro-container">
       <h1>Learn about the planets</h1>
       <p>One quiz at a time</p>
       <div class="startpage-btn-part">
-      <input type="button" value="Go to quiz" class="btn-quiz">
-      <input type="button" value="Read more" class="btn-more">
+        <router-link to="/quiz">
+          <button class="btn-quiz">Go to quiz</button>
+        </router-link>
+        <router-link to="/facts">
+          <button class="btn-more">Read more</button>
+        </router-link>
+      </div>
+      <div class="startpage-background-planet">
+        <img :src="earthImg" class="earth-img">
       </div>
     </div>
     <div class="startpage-sellingpoints-container">
-      <img src="" alt="Fact 1">
+      <img :src="star" alt="Fact 1" class="star">
       <p>Gamified learning</p>
-      <img src="" alt="Fact 2">
+      <img :src="star" alt="Fact 2" class="star">
       <p>Bite-sized, anytime learning</p>
-      <img src="" alt="Fact 3">
+      <img :src="star" alt="Fact 3" class="star">
       <p>Daily challenges</p>
     </div>
   </main>
 </template>
 
-<style>
+<style scoped>
 .startpage-intro-container {
-  margin-top: 3em;
+  margin-top: 7vh;
+  margin-left: 2vw;
   padding: 3em;
 }
 
-.btn-quiz, .btn-more {
-  margin: 2em 2em 0 0;
-  padding: .5em;
+.startpage-background-planet {
+  position: absolute;
+  top: 6vh;
+  left: 60vw;
+}
+
+.earth-img {
+  height: 70vh;
+}
+
+.star {
+  height: 5vh;
+}
+
+p {
+  margin-top: 2vh;
+  margin-bottom: 2vh;
+}
+
+.startpage-btn-part {
+  margin-top: 4vh;
+}
+
+.btn-quiz {
+  background-color: #e3cafb;
+  border: none;
+  width: 140px;
+  height: 43.21px;
+  border-radius: 36.06px;
+  color: #40027d;
+  font-weight: 550;
   cursor: pointer;
+}
+
+.btn-quiz:hover {
+  background-color: #27006f;
+  color: #e3cafb;
+  border: solid 2px #e3cafb;
+}
+
+
+.btn-more {
+  background-color: #27006f00;
+  border: solid 2px #e3cafb;
+  width: 140px;
+  height: 43.21px;
+  border-radius: 36.06px;
+  color: #e3cafb;
+  font-weight: 550;
+  cursor: pointer;
+  margin-left: 4vw;
+}
+
+.btn-more:hover {
+  background-color: #e3cafb;
+  color: #27006f;
 }
 
 .startpage-sellingpoints-container {
@@ -36,5 +107,6 @@
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
   padding: 6em;
+  margin-bottom: 3em;
 }
 </style>
