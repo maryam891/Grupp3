8cb4f3d039aa5a51e8ef30b267012d923f51b843
Maryam <maryam.chaudhry@iths.se>
2025-02-24 11:40:54 +0100

    ändrar så att länken stämmer igen i router js och i navbar

--- Grupp3/src/router.js
+++ Grupp3/src/router.js
@@ -3,14 +3,14 @@ import HomeView from "./views/HomeView.vue";
 import FactsView from "./views/FactsView.vue";
 import QuizShow from "./views/QuizShow.vue";
 import ContactView from "./views/ContactView.vue";
-import favorites from "./views/Favorites.vue";
+import Favorites from "./views/Favorites.vue";
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: "/", component: HomeView },
     { path: "/facts", component: FactsView },
     { path: "/quiz", component: QuizShow },
-    { path: "/favorites", favorites },
+    { path: "/Favorites", Favorites },
     { path: "/contact", component: ContactView },
   ],
 });
