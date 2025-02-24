911686b734f76540ee5798d7cd3ebaa5f60cbed1
Janefur <a.janefur@gmail.com>
2025-02-24 13:57:42 +0100

    fixar bilder och funktioner på FactsView sidan

--- Grupp3/src/views/FactsView.vue
+++ Grupp3/src/views/FactsView.vue
@@ -1,33 +1,48 @@
 <script>
 export default {
-  // async created() {
-  //      const res = await fetch('http://localhost:5000/quiz')
-  //      const data = await res.json()
-  //      return data
-  //   },
   created(){
     fetch('/infodb.json')
     .then(response => response.json())
     .then(data =>{
-      // this.planetInfo = data.name
-      // console.log(data.planets.mars)
-      console.log(data)
-      console.log(data.planet_info[0].name)
+      this.planets = data
+      console.log("planets", data)
+      console.log(data[0].name)
+
+      //skapar en array med bilder, där jag hämtar info till respektive planet från infodb.json. Går det att göra så?
+      this.images= [
+      {src: '/assets/image/earth-transparent.png', text: this.planets[2].info, name: this.planets[2].name},
+      {src: '/assets/image/mercury.png', text: this.planets[0].info, name: this.planets[0].name}
+      ]
+
     })
   },
   data() {
     return {
-      planetsName: null,
+      planets: null,
       planetInfo: null,
-      imageMars: "/assets/image/mars-transparent.png"
+      imageMars: "/assets/image/mars-transparent.png",
+      modalVisible: false,
+      modalText: '',
+      modalHeader: '',
+      images:[]
+
     };
   },
   methods:{
-    mouseOver(){
-      this.$refs.imageMars.classList.add("hovered")
+    mouseOver(event){
+      event.target.classList.add("hovered")
     },
-    mouseOut(){
-      this.$refs.imageMars.classList.remove("hovered")
+    mouseOut(event){
+      event.target.classList.remove("hovered")
+    },
+
+    showModal(image){
+      this.modalVisible = true;
+      this.modalText = image.text;
+      this.modalHeader = image.name;
+    },
+    closeModal(){
+      this.modalVisible = false;
     }
   }
 };
@@ -35,21 +50,44 @@ export default {
 
 <template>
   <div class="main">
-    <h1>Some fun facts about each planet in our solar system (including Pluto)</h1>
-    <p>Click the planet to read more</p>
-    <img :src= imageMars alt="" @mouseover="mouseOver" @mouseout="mouseOut" ref="imageMars">
     <div class="information">
+      <h1>Some fun facts about each planet in our solar system (including Pluto)</h1>
+      <p>Click the planet to read more</p>
 
-      <div class="planet-info">
-      <h2></h2>
-      <p></p>
-      </div>
+      <div class="planet-card"><!--När jag klickar på bilden ska funktionen showModal "starta" som innehåller info från arrayen i data-->
+        <!-- <img :src= imageMars alt="" @mouseover="mouseOver" @mouseout="mouseOut" @click="showModal(image)" ref="imageMars">
+        <div v-if="planets !== null">
+          {{ planets[0].name }}
+        </div> -->
 
-      <p>science.nasa.gov (källa)</p>
-
+        <!--Försöker rendera bilderna från arrayen i created-->
+        <div class="planets">
+          <div class="planet-card" v-for="(image, index) in images" :key="index" >
+            <img :src="image.src" :alt="image.name"  @click="showModal(image)" @mouseover="mouseOver" @mouseout="mouseOut">
+            <p> {{ image.name }}</p>
+          </div>
+        </div>
     </div>
-  </div>
+      <p>science.nasa.gov (källa)</p>
+    </div>
 
+<!--modal kod-->
+    <div class="modal" v-show="modalVisible">
+      <div class="modal-inner">
+
+        <header class="modal-header">
+          {{ modalHeader }}
+        </header>
+
+        <section class="modal-body">
+          {{ modalText }}
+        </section>
+
+        <button class="btn-close" @click="closeModal">Close</button>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
@@ -60,4 +98,28 @@ export default {
   .main{
     padding: 3em;
   }
+
+.modal{
+  position: fixed;
+  top:0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0,2);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-inner{
+  background: #464545;
+  padding: 32px;
+}
+
+.planets{
+  display: flex;
+}
 </style>
